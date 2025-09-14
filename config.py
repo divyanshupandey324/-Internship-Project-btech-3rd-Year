12f9b4166654c90c279b296fc669a5dadd9af13e
divyanshupandey324 <divyanshu.p894@gmail.com>
2025-09-15 01:46:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     # Use an environment variable for the database URI
     # Clever Cloud will provide this. You'll set it as an environment variable in Render.
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@localhost/dbname"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     UPLOAD_FOLDER = 'static/uploads'
