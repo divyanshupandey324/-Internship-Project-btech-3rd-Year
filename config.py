c5e8027180050f6d66dd9a817ac8c3b836c56d46
divyanshupandey324 <divyanshu.p894@gmail.com>
2025-09-15 02:06:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@host:3306/dbname"
+    SQLALCHEMY_DATABASE_URI = (
+    "mysql+pymysql://ufquegmkfvzxgklh:di57GgsCPzk0iVOcCDuI@butxnm9ctrz7w0z06p2a-mysql.services.clever-cloud.com:3306/butxnm9ctrz7w0z06p2a"
+    )
+
 
     UPLOAD_FOLDER = "static/uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
