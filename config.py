bcb71dfbfb26f8f6fc6a3bd92d9274e9c6cb03ec
divyanshupandey324 <divyanshu.p894@gmail.com>
2025-09-15 01:54:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
+
+
+
 import os
 
 class Config:
-    # Use an environment variable for the database URI
-    # Clever Cloud will provide this. You'll set it as an environment variable in Render.
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "mysql+pymysql://ufquegmkfvzxgklh:di57GgsCPzk0iVOcCDuI@butxnm9ctrz7w0z06p2a-mysql.services.clever-cloud.com:3306/butxnm9ctrz7w0z06p2a"
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        "mysql+pymysql://defaultuser:defaultpass@localhost/dbname"
     )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    UPLOAD_FOLDER = 'static/uploads'
+    UPLOAD_FOLDER = "static/uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
