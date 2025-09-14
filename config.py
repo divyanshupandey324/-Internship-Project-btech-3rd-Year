e2f773f70aa5a11effa28b51179dcaaf30d43f44
divyanshupandey324 <divyanshu.p894@gmail.com>
2025-09-15 01:51:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     # Use an environment variable for the database URI
     # Clever Cloud will provide this. You'll set it as an environment variable in Render.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
     "mysql+pymysql://ufquegmkfvzxgklh:di57GgsCPzk0iVOcCDuI@butxnm9ctrz7w0z06p2a-mysql.services.clever-cloud.com:3306/butxnm9ctrz7w0z06p2a"
-)
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
