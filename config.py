91206b6fa411938002c586e34717f0cd9c2e2a24
divyanshupandey324 <divyanshu.p894@gmail.com>
2025-09-15 02:02:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URI",
-        "mysql+pymysql://defaultuser:defaultpass@localhost/dbname"
-    )
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@host:3306/dbname"
+
     UPLOAD_FOLDER = "static/uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
